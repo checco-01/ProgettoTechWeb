c4911c878eda6524c5f38d97e9c6b3f548c9990c
checco-01 <francescomiele0@gmail.com>
2026-06-03 13:12:48 +0200

    Initial Commit

---
+++ roadToUninaBackend/src/main/java/techWeb/roadToUninaBackend/RoadToUninaBackendApplication.java
@@ -0,0 +1,13 @@
+package techWeb.roadToUninaBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToUninaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadToUninaBackendApplication.class, args);
+	}
+
+}
---
+++ roadToUninaBackend/src/test/java/techWeb/roadToUninaBackend/RoadToUninaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package techWeb.roadToUninaBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadToUninaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
