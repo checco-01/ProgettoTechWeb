5125cd5fedc9a6f53042f962b7bbaffc7eab57e4
checco-01 <francescomiele0@gmail.com>
2026-06-05 21:38:19 +0200

    Prima implementazione Entità

---
+++ roadToUninaBackend/src/main/java/techWeb/roadToUninaBackend/repository/GameRepository.java
@@ -0,0 +1,15 @@
+﻿package techWeb.roadToUninaBackend.repository;
+
+import techWeb.roadToUninaBackend.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findByUserIdOrderByCreatedAtDesc(Long userId);
+    List<Game> findByCompletedTrueOrderByTimeElapsedSecondsAsc();
+    List<Game> findByCompletedTrueOrderByNumberOfStepsAsc();
+    List<Game> findByCompletedTrueAndUserIdOrderByCreatedAtDesc(Long userId);
+}
---
+++ roadToUninaBackend/src/main/java/techWeb/roadToUninaBackend/repository/GameStepRepository.java
@@ -0,0 +1,12 @@
+﻿package techWeb.roadToUninaBackend.repository;
+
+import techWeb.roadToUninaBackend.model.GameStep;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GameStepRepository extends JpaRepository<GameStep, Long> {
+    List<GameStep> findByGameIdOrderByStepNumberAsc(Long gameId);
+}
