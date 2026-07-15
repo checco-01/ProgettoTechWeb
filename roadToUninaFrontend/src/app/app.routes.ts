c88fa01a4da9faa2708bdf6fcd24dd330a1cff26
checco-01 <francescomiele0@gmail.com>
2026-07-15 20:11:52 +0200

    fix su log in

--- roadToUninaFrontend/src/app/app.routes.ts
+++ roadToUninaFrontend/src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./pages/register/register').then((m) => m.RegisterComponent),
+  },
+  {
+    path: 'game',
+    canActivate: [authGuard],
+    // Quando creerai GameComponent, sostituisci con:
+    // loadComponent: () => import('./pages/game/game').then(m => m.GameComponent),
+    loadComponent: () =>
+      import('./pages/login/login').then((m) => m.LoginComponent),
+  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login' },
+];
